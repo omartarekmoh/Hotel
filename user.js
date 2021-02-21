afd686585b27ae14805af0265f743c3b666b3617
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-21 17:07:45 +0200

    remove old files

--- user.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$('#search').keyup(function () {
-		var value = $('#search').val();
-		console.log(value);
-		$('#rr').load('get_user.php', {
-			value: value,
-		});
-	});
-});
